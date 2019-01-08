db64ed6e2e62d619979fbf419367190e01da9ee0
jonmoss <jonathan@castlegateit.co.uk>
2019-01-08 15:37:58 +0000

    Improve documentation.

--- includes/organization.php
+++ includes/organization.php
@@ -4,7 +4,7 @@ use Spatie\SchemaOrg\Schema;
 
 $organizationSchema = Schema::organization();
 
-// Iterate over properties defined in config.php and
+// Iterate over properties defined in config.php and build up the structured data for output.
 foreach ($config_schema as $schema_property => $schema_property_value) {
     if($schema_property_value) {
         $organizationSchema->$schema_property($schema_property_value);
